1883cc554f1fa9a7259216bdaa8c82786f6f5602
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-10-24 19:21:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function updateTimer(){
+const timer = document.getElementById("timer");
+const date = new Date();
+const dateOptions = { month: 'numeric',   day: 'numeric', year: 'numeric' };
+const timeOptions = { hour: '2-digit', minute: '2-digit', second: '2-digit'};
+
+const formattedDate = date.toLocaleDateString(undefined, dateOptions);
+const formattedTime = date.toLocaleTimeString(undefined, timeOptions);
+
+timer.textContent = `${formattedDate}, ${formattedTime.toUpperCase()}`;
+
+}
+updateTimer();
+ 
+setInterval(updateTimer, 1000);
